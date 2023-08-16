26ec675b6ac243807949e98b91c54cd6c163146a
michael-modlee <michael@modlee.ai>
2023-08-16 18:43:31 -0400

    Modify simple_torch.py to remove GPU

--- modlee_pypi/utils.py
+++ modlee_pypi/utils.py
@@ -1,5 +1,8 @@
 import os
 import pathlib
+import torch
+os.environ['PYTORCH_ENABLE_MPS_FALLBACK']='1'
+
 
 def safe_mkdir(target_dir):
     root,ext = os.path.splitext(target_dir)
@@ -12,3 +15,15 @@ def safe_mkdir(target_dir):
     #     target_dir,_ = os.path.split(target_dir.split('.')[0])
     if not os.path.exists(target_dir):
         os.mkdir(target_dir)
+        
+        
+def set_torch_device():
+    if torch.cuda.is_available():
+        torch.set_default_device('cuda')
+        return torch.device('cuda')
+    elif torch.backends.mps.is_available():
+        torch.set_default_device('mps')
+        return torch.device('mps')
+    else:
+        torch.set_default_device('cpu')
+        return torch.device('cpu')
