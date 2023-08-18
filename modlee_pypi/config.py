899643cc4de1facc4cf6be07edf3121e27702efc
michael-modlee <michael@modlee.ai>
2023-08-17 20:17:40 -0400

    Prototyping API client

--- modlee_pypi/config.py
+++
@@ -1,4 +0,0 @@
-from pathlib import Path
-ROOT_DIR = Path('./').absolute()
-PYPI_DIR = ROOT_DIR / 'modlee_pypi'
-TMP_DIR = ROOT_DIR / 'tmp'
--- modlee_pypi/experiments.py
+++
@@ -1,3 +0,0 @@
-# given data, calculate stats, combine with target requirements to create a prompt
-
-# def document_experiment(data=None,data_stats_func=None,requirements=None,model=None,data_transform_func=None,evaluate_func=None,performance=None):
