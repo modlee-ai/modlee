5542786130399ba74a8330b9ef002747ebfb3dfd
Brad Magnetta <brad@Brads-MacBook-Air.local>
2023-10-21 10:33:36 -0400

    rebuilt code to get_model_details

--- modlee/recommender.py
+++ modlee/recommender.py
@@ -16,6 +16,7 @@ from modlee.utils import get_model_size
 import requests
 from modlee.utils import get_model_size
 import requests
+from modlee.utils import get_model_size
 modlee_converter = Converter()
 logging.basicConfig(level=logging.INFO)
 
