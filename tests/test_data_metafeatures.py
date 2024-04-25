40db9dfaa62570628903b894f5869f428cb26a3b
Michael Suguitan <michael@modlee.ai>
2024-04-25 23:39:22 +0000

    Text and image metafeature analysis

--- tests/test_data_metafeatures.py
+++ tests/test_data_metafeatures.py
@@ -1,5 +1,5 @@
 import pytest
-import re
+import os, re
 
 import torch
 from torch.utils.data import DataLoader
@@ -11,22 +11,26 @@ from modlee.utils import image_loaders, text_loaders
 
 import spacy
 
+DATA_ROOT = os.path.expanduser("~/efs/.data")
+
 IMAGE_DATALOADER = modlee.utils.get_imagenette_dataloader()
 # TEXT_DATALOADER = modlee.utils.get_wnli_dataloader() 
 
-
-IMAGE_LOADERS = {loader_fn:getattr(image_loaders, loader_fn) for loader_fn in dir(image_loaders) if re.match('get_(.*)_dataloader', loader_fn)}
+IMAGE_LOADERS = {loader_fn:getattr(image_loaders, loader_fn) for loader_fn in sorted(dir(image_loaders)) if re.match('get_(.*)_dataloader', loader_fn)}
 TEXT_LOADERS = {loader_fn:getattr(text_loaders, loader_fn) for loader_fn in dir(text_loaders) if re.match('get_(.*)_dataloader', loader_fn)}
 
+print('\n'.join(f"image loader{i}: {image_loader}" for i, image_loader in enumerate(IMAGE_LOADERS)))
 import pandas as pd 
 # df = pd.DataFrame()
 df = None
+
 
 class TestDataMetafeatures:
     
     @pytest.mark.parametrize('get_dataloader_fn', IMAGE_LOADERS.values())
     def test_image_dataloader(self, get_dataloader_fn):
-        image_mf = dmf.ImageDataMetafeatures(get_dataloader_fn(), testing=True)
+        image_mf = dmf.ImageDataMetafeatures(
+            get_dataloader_fn(root=DATA_ROOT), testing=True)
         self._check_has_metafeatures(image_mf)
 
     # @pytest.mark.parametrize('get_dataloader_fn', [
@@ -37,7 +41,8 @@ class TestDataMetafeatures:
     # @pytest.mark.parametrize('get_dataloader_fn', [modlee.utils.get_wnli_dataloader])
     @pytest.mark.parametrize('get_dataloader_fn', TEXT_LOADERS.values())
     def test_text_dataloader(self, get_dataloader_fn):
-        text_mf = dmf.TextDataMetafeatures(get_dataloader_fn(), testing=True)
+        text_mf = dmf.TextDataMetafeatures(
+            get_dataloader_fn(root=DATA_ROOT), testing=True)
         self._check_has_metafeatures(text_mf)
 
     def _check_has_metafeatures(self, mf): 
@@ -53,19 +58,4 @@ class TestDataMetafeatures:
             # Assert that the attribute is a flat dictionary
             assert not any([isinstance(v,dict) for v in getattr(mf, metafeature_type).values()]), f"{mf} {metafeature_type} not a flat dictionary"
             features.update(getattr(mf, metafeature_type))
-            
-        global df
-        # breakpoint()
-        if df is None:
-            df = pd.DataFrame(features)
-        else:
-            try:
-                df = pd.concat([df, pd.DataFrame(features)]).reset_index()
-            except:
-                pass
-            # df = df.append(features, ignore_index=True)
-            # df.update(features)
-        # breakpoint()
-        # breakpoint()
-        
-    
+           
