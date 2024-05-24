9bb3611c02c6f68daa83b6199c002f6d9de598f7
Michael Suguitan <michael@modlee.ai>
2024-05-24 18:07:49 +0000

    Integrated data and model metafeatures with graph handling into package proper

--- src/modlee/dataframes.py
+++ src/modlee/dataframes.py
@@ -3,7 +3,14 @@ import numpy as np
 from functools import partial
 
 class ModleeDataFrame(pd.DataFrame):
+    """
+    A wrapper class over a pandas DataFrame with convenience functions for metafeatures.
+    """
     def to_tsv(self, *args, **kwargs):
+        """
+        Save a dataframe to a tab-separated value file.
+        Wraps around and passes arguments to pd.DataFrame.to_csv().
+        """
         super().to_csv(
             *args, 
             sep="\t",
@@ -13,6 +20,14 @@ class ModleeDataFrame(pd.DataFrame):
         )
 
     def to_txt(self, path, columns=None, apply_fn=None, *args, **kwargs):
+        """
+        Save a column of a dataframe 
+
+        :param path: The path to save the txt to.
+        :param columns: The columns of the dataframe to save, defaults to None
+        :param apply_fn: An optional function to apply to the dataframe (i.e. pd.DataFrame.apply(apply_fn)), defaults to None
+        """
+
         df_txt = self
         if columns is not None:
             df_txt = df_txt[columns]
@@ -25,6 +40,10 @@ class ModleeDataFrame(pd.DataFrame):
         
 
 class DataFrameTransforms:
+    """
+    Transforms for a dataframe.
+    Inspired by torchvision's transforms.
+    """
     @staticmethod
     def list_cols2item(df):
         object_columns = df.select_dtypes(include=['object']).columns
