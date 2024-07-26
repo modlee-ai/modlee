dfc09c34686332d375b73f52b8bec417d86608b0
Michael Suguitan <michael@modlee.ai>
2024-07-26 20:19:33 +0000

    NB201 benchmarking

--- src/modlee/converter.py
+++ src/modlee/converter.py
@@ -604,10 +604,6 @@ class Converter(object):
                 continue
             if len(value_shape)==0:
                 continue
-            if len(value_shape)==0:
-                continue
-            if len(value_shape)==0:
-                continue
             if isinstance(value_shape[0], str):
                 if "dynamic_axes" in value_shape[0]:
                     continue
